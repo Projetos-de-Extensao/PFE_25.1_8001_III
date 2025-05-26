59a50c174a6b2d7cf6fee58452b6a929c144cd0d
João Victor de Carvalho <81653692+JVictor-De@users.noreply.github.com>
2025-05-26 19:55:58 -0300

    create pages jsx

---
+++ musicVibe/pages/pages/pesquisa.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+function Pesquisa() {
+  return (
+    <section className="top-picks">
+      <div className="container">
+        <div className="top-picks-filter">
+          <h2>Principais eventos Perto de Você</h2>
+          <button className="btn btn-outline">
+            <img src="src/filter.png" alt="" /> Filtrar
+          </button>
+        </div>
+        <hr />
+        <div className="event-list">
+          <div className="event">
+            <img src="src/Rectangle 1363.png" alt="" />
+            <div className="event-details">
+              <p>13 de Ago</p>
+              <p>Dom - 10:00 AM</p>
+              <p>Elements Music and Arts Festival - Domingo</p>
+            </div>
+            <div className="event-actions">
+              <div className="btn-picks">
+                <button className="btn btn-outline">Ver Detalhes</button>
+                <button className="btn">Reservar Agora</button>
+              </div>
+            </div>
+          </div>
+          <div className="event">
+            <img src="src/Rectangle 1363-1.png" alt="" />
+            <div className="event-details">
+              <p>13 de Ago</p>
+              <p>Dom - 10:00 AM</p>
+              <p>Elements Music and Arts Festival - Domingo</p>
+            </div>
+            <div className="event-actions">
+              <div className="btn-picks">
+                <button className="btn btn-outline">Ver Detalhes</button>
+                <button className="btn">Reservar Agora</button>
+              </div>
+            </div>
+          </div>
+          <div className="event">
+            <img src="src/Rectangle 1363-2.png" alt="" />
+            <div className="event-details">
+              <p>13 de Ago</p>
+              <p>Dom - 10:00 AM</p>
+              <p>Elements Music and Arts Festival - Domingo</p>
+            </div>
+            <div className="event-actions">
+              <div className="btn-picks">
+                <button className="btn btn-outline">Ver Detalhes</button>
+                <button className="btn">Reservar Agora</button>
+              </div>
+            </div>
+          </div>
+          <div className="event">
+            <img src="src/Rectangle 1361-3.png" alt="" />
+            <div className="event-details">
+              <p>13 de Ago</p>
+              <p>Dom - 10:00 AM</p>
+              <p>Elements Music and Arts Festival - Domingo</p>
+            </div>
+            <div className="event-actions">
+              <div className="btn-picks">
+                <button className="btn btn-outline">Ver Detalhes</button>
+                <button className="btn">Reservar Agora</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="src/fundo_branco_degrade.png" alt="" />
+        <button className="btn btn-outline show-more-btn">Mostrar Mais</button>
+      </div>
+    </section>
+  );
+}
+
+export default Pesquisa;
