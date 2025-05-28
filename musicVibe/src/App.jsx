0f876c18a4a401fbd5e38171d7d8ba36a358a966
João Victor de Carvalho <81653692+JVictor-De@users.noreply.github.com>
2025-05-28 13:19:26 -0300

    Add files via upload

--- musicVibe/src/App.jsx
+++ musicVibe/src/App.jsx
@@ -1,36 +1,33 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import './componentes/Cadastro/cadastro.css'
-import './componentes/Header/Header.css'
-import './componentes/Footer/footer.css'
-import './componentes/List/List.css'
-import './componentes/Category/Category.css'
-import Banner from './componentes/Banner/Banner'
-import Cadastro from './componentes/Cadastro/Cadastro'
 import Header from './componentes/Header/Header'
-import Footer from './componentes/Footer/Footer'
-import List from './componentes/List/List'
-import Category from './componentes/Category/Category' 
-import Home from './pages/Home'
+import Footer from './componentes/Footer/Footer.jsx'
 
-
-
+import Home from '../pages/Home.jsx'
+import Sobre from '../pages/Sobre.jsx'
+import Cadastro from '../pages/Cadastro.jsx'
+import CadastroEvent from '../pages/CadastroEvent.jsx'
+import Carinho from '../pages/Carinho.jsx'
+import Pesquisa from '../pages/Pesquisa.jsx'
+import User from '../pages/User.jsx'
+import Event from '../pages/Event.jsx' 
 function App() {
   return (
-    <>
-    <Home />
-    
-    {/*<Banner/>
-
-    <Category/>
-
-    <List/>
-
-    <Footer/>*/}
-
-
- 
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/cadastro-evento" element={<CadastroEvent />} />
+        <Route path="/carrinho" element={<Carinho />} />
+        <Route path="/pesquisa" element={<Pesquisa />} />
+        <Route path="/evento" element={<Event />} />
+        <Route path="/user" element={<User />} />
+        
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
