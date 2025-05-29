39b561c615c6c981fb96dfa003c8b89d6d48fc8b
João Victor de Carvalho <81653692+JVictor-De@users.noreply.github.com>
2025-05-28 23:32:14 -0300

    Reorganization of src folder.

--- musicVibe/src/App.jsx
+++ musicVibe/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './componentes/Footer/Footer.jsx'
 import Home from '../pages/Home.jsx'
 import Sobre from '../pages/Sobre.jsx'
 import Cadastro from '../pages/Cadastro.jsx'
-import CadastroEvent from '../pages/CadastroEvent.jsx'
+import Cadastroevent from '../pages/CadastroEvent.jsx'
 import Carinho from '../pages/Carinho.jsx'
 import Pesquisa from '../pages/Pesquisa.jsx'
 import User from '../pages/User.jsx'
@@ -19,7 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/cadastro-evento" element={<CadastroEvent />} />
+        <Route path="/cadastro-evento" element={<Cadastroevent />} />
         <Route path="/carrinho" element={<Carinho />} />
         <Route path="/pesquisa" element={<Pesquisa />} />
         <Route path="/evento" element={<Event />} />
