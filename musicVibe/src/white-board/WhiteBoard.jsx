78fca0557933d2144d57b989448d169f685df4d3
João Victor de Carvalho <81653692+JVictor-De@users.noreply.github.com>
2025-05-29 12:38:39 -0300

    page alignment and json file implementation

---
+++ musicVibe/src/white-board/WhiteBoard.jsx
@@ -0,0 +1,9 @@
+export default function WhiteBoard() {
+    return (
+        <div className="white-background">
+            <button className="btn btn-outline show-more-btn white-background-bottom">
+                Mostrar Mais
+            </button>
+        </div>
+    );
+}
