260ae93ea7e6c74f1436b766edf11b29071f6342
João Victor de Carvalho <81653692+JVictor-De@users.noreply.github.com>
2025-06-09 12:14:34 -0300

    reorganation and trasg white-board

--- musicVibe/src/componentes/white-board/WhiteBoard.jsx
+++
@@ -1,9 +0,0 @@
-export default function WhiteBoard() {
-    return (
-        <div className="white-background">
-            <button className="btn btn-outline show-more-btn white-background-bottom">
-                Mostrar Mais
-            </button>
-        </div>
-    );
-}
